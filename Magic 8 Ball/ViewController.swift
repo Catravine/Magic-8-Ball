44d3c26a504b0a6c69d4cad62ba3141eaaeb7648
Caroline Courtney <caroline@carolinecourtney.com>
2018-09-02 13:57:39 -0400

    finished Magic 8 Ball coding challenge; shows different ball image when ask is clicked, or when shaken

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
+    var randomBallNumber : Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        newBallImage()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func askButtonPressed(_ sender: Any) {
+        newBallImage()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
+    
+    func newBallImage() {
+        randomBallNumber = Int.random(in: 0 ... 4)
+        imageView.image = UIImage.init(named: ballArray[randomBallNumber])
+    }
 }
 
