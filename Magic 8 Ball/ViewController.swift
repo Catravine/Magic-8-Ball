8b2346e05394872159d38b4746a673c37933cb01
Caroline Courtney <caroline@carolinecourtney.com>
2018-09-02 13:29:47 -0400

    initial commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Caroline Courtney on 9/2/18.
+//  Copyright © 2018 Caroline Courtney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
